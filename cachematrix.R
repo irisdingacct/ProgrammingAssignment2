d6cbbff2e2ef5fb703157983d6291d02796a6e23
IrisD <71967914+irisdingacct@users.noreply.github.com>
2020-10-18 21:20:38 +1100

    ## Programming Assignment 2 : Inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##m1 with simple matrix inverse A
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow =2, ncol = 2)
+m1
 
+##Creating matrix that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+          m <- NULL
+          set <- function(y) {
+                    x <<- y
+                    m <<- NULL
+          }
+          get <- function() x
+          setinverse <- function(inverse) m <<- inverse
+          getinverse <- function() m
+          list(set = set, 
+               get = get,
+               setinverse = setinverse, 
+               getinverse = getinverse)
+          
 }
 
-
-## Write a short comment describing this function
-
+## Computes the inverse of the special matrix , if the inverse has already been calculated, retrieve from cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          m <- x$getinverse()
+          if(!is.null(m)) {
+                    message("getting cached data")
+                    return(m)
+          }
+          data <- x$get()
+          m <- solve(data, ...)
+          x$setinverse(m)
+          m
+          ## Return a matrix that is the inverse of 'x'
 }
+
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow =2, ncol = 2)
+
+A <- makeCacheMatrix(m1)
+
+
+##Calculate Inverse
+cacheSolve(A)
+##Getting cached data
+cacheSolve(A)
+
